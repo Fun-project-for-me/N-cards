cde06578b25a5b7b64383fc68bac86f01b71469b
Fun-project-for-me <sorry272006@gmail.com>
2025-07-23 22:34:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,4 +115,27 @@ showCardBtn.addEventListener("click", () => {
   showCardBtn.style.display = "none";
   msg1.style.display = "none";
   msg2.style.display = "none";
+var duration = 3 * 1000;
+var end = Date.now() + duration;
+
+(function frame() {
+  confetti({
+    particleCount: 5,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 }
+  });
+  confetti({
+    particleCount: 5,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 }
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+})();
+
+  
 });
